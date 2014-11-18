b00e2aa31bcb2c84c24ab78976a5f5e974c8b9f8
Gary Chang <gary.chang.355@gmail.com>
2014-11-17 20:13:26 -0500

    Erased some commented code

--- src/OptimizedAlgorithm.py
+++ src/OptimizedAlgorithm.py
@@ -132,28 +132,6 @@ def main(argv=None):
             print ''.join(text)
             print ''
 
-    # while count < 1000:
-    #     a = random.randint(0,25)
-    #     b = random.randint(0,25)
-    #     child = parentkey[:]
-    #     # swap two characters in the child
-    #     child[a],child[b] = child[b],child[a]
-    #     deciphered = SimpleSub(child).decipher(ctext)
-    #     score = fitness.score(deciphered)
-    #     # if the child was better, replace the parent with it
-    #     if score > parentscore:
-    #         parentscore = score
-    #         parentkey = child[:]
-    #         count = 0
-    #     count = count+1
-    # # keep track of best score seen so far
-    # if parentscore>maxscore:
-    #     maxscore,maxkey = parentscore,parentkey[:]
-    #     print '\nbest score so far:',maxscore,'on iteration',i
-    #     ss = SimpleSub(maxkey)
-    #     print '    best key: '+''.join(maxkey)
-    #     print '    plaintext: '+ss.decipher(ctext)
-
 if __name__ == "__main__":
     sys.exit(main())
 
